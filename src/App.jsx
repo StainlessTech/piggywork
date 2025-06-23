ba3e5c80c7f3364916bad3964e3b1dc7ad870bde
Onifade Oluwasegun Samson <oluwasegunstainless@gmail.com>
2025-06-23 17:16:14 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./static/Header";
+
+const App = () => {
+  return (
+    <div className="bg-amber-600">
+      <h1>Getting Started</h1>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
